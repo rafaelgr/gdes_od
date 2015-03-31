62b585e1841a08238bc48f8862ee69827a966d38
rafaelgr <rafa@myariadna.com>
2015-03-31 19:04:39 +0200

    Objetivos 2
    Mantenimiento completo

---
+++ public/js/ariadna/objetivoDetalle.js
@@ -0,0 +1,181 @@
+﻿/*-------------------------------------------------------------------------- 
+objetivoDetalle.js
+Funciones js par la página ObjetivoDetalle.html
+---------------------------------------------------------------------------*/
+var objetivoId = 0; 
+function initForm() {
+    // comprobarLogin();
+    // de smart admin
+    pageSetUp();
+    // 
+    getVersionFooter();
+    vm = new objetivoData();
+    ko.applyBindings(vm);
+    // asignación de eventos al clic
+    $("#btnAceptar").click(aceptar());
+    $("#btnSalir").click(salir());
+    $("#frmObjetivo").submit(function () {
+        return false;
+    });
+    
+    
+
+    objetivoId = gup('ObjetivoId');
+    if (objetivoId != 0) {
+        var data = {
+            objetivoId: objetivoId
+        }
+        // hay que buscar ese elemento en concreto
+        $.ajax({
+            type: "GET",
+            url: "/api/objetivos/" + objetivoId,
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            success: function (data, status) {
+                // hay que mostrarlo en la zona de datos
+                loadData(data);
+            },
+            error: errorAjax
+        });
+    } else {
+        // se trata de un alta ponemos el id a cero para indicarlo.
+        vm.objetivoId(0);
+        loadCategorias(-1);
+        loadTipos(-1);
+    }
+}
+
+function objetivoData() {
+    var self = this;
+    self.objetivoId = ko.observable();
+    self.nombre = ko.observable();
+    self.categoria = ko.observable();
+    self.tipo = ko.observable();
+    // soporte de combos
+    self.posiblesCategorias = ko.observableArray([]);
+    self.posiblesTipos = ko.observableArray([]);
+    // valores escogidos
+    self.scategoriaId = ko.observable();
+    self.stipoId = ko.observable()
+}
+
+function loadData(data) {
+    vm.objetivoId(data.objetivoId);
+    vm.nombre(data.nombre);
+    vm.categoria(data.categoria);
+    vm.tipo(data.tipo);
+    loadCategorias(data.categoria.categoriaId);
+    loadTipos(data.tipo.tipoId);
+}
+
+function loadCategorias(categoriaId){
+    $.ajax({
+        type: "GET",
+        url: "/api/categorias",
+        dataType: "json",
+        contentType: "application/json",
+        success: function (data, status){
+            vm.posiblesCategorias(data);
+            vm.scategoriaId(categoriaId);
+        },
+        error: errorAjax
+    });
+}
+
+function loadTipos(tipoId) {
+    $.ajax({
+        type: "GET",
+        url: "/api/tipos",
+        dataType: "json",
+        contentType: "application/json",
+        success: function (data, status) {
+            vm.posiblesTipos(data);
+            vm.stipoId(tipoId);
+        },
+        error: errorAjax
+    });
+}
+
+function datosOK() {
+    $('#frmObjetivo').validate({
+        rules: {
+            txtNombre: { required: true },
+            cmbCategorias: { required: true },
+            cmbTipos: { required: true }
+        },
+        // Messages for form validation
+        messages: {
+            txtNombre: {required: 'Introduzca el nombre'},
+            cmbCategorias: {required: 'Seleccione una categoría'},
+            cmbTipos: {required: 'Seleccione un tipo'}
+        },
+        // Do not change code below
+        errorPlacement: function (error, element) {
+            error.insertAfter(element.parent());
+        }
+    });
+    //// comprobamos que ha seleccionado al menos una categoría y un tipo
+    //if (vm.scategoriaId() < 0 || vm.stipoId() < 0) {
+    //    mostrarMensajeSmart("Debe seleccionar una categoría y un tipo");
+    //    return false;
+    //}
+    return $('#frmObjetivo').valid();
+}
+
+function aceptar() {
+    var mf = function () {
+        if (!datosOK())
+            return;
+        var data = {
+            objetivo: {
+                "objetivoId": vm.objetivoId(),
+                "nombre": vm.nombre(),
+                "categoria": {
+                    "categoriaId": vm.scategoriaId()
+                },
+                "tipo": {
+                    "tipoId": vm.stipoId()
+                }
+            }
+        };
+        if (objetivoId == 0) {
+            $.ajax({
+                type: "POST",
+                url: "api/objetivos",
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function (data, status) {
+                    // Nos volvemos al general
+                    var url = "ObjetivoGeneral.html?ObjetivoId=" + data.objetivoId;
+                    window.open(url, '_self');
+                },
+                error: errorAjax
+            });
+        } else {
+            $.ajax({
+                type: "PUT",
+                url: "api/objetivos/" + objetivoId,
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function (data, status) {
+                    // Nos volvemos al general
+                    var url = "ObjetivoGeneral.html?ObjetivoId=" + data.objetivoId;
+                    window.open(url, '_self');
+                },
+                error: errorAjax
+            });
+        }
+    };
+    return mf;
+}
+
+function salir() {
+    var mf = function () {
+        var url = "ObjetivoGeneral.html";
+        window.open(url, '_self');
+    }
+    return mf;
+}
---
+++ public/js/ariadna/objetivoGeneral.js
@@ -0,0 +1,222 @@
+﻿/*-------------------------------------------------------------------------- 
+objetivoGeneral.js
+Funciones js par la página ObjetivoGeneral.html
+
+---------------------------------------------------------------------------*/
+var responsiveHelper_dt_basic = undefined;
+var responsiveHelper_datatable_fixed_column = undefined;
+var responsiveHelper_datatable_col_reorder = undefined;
+var responsiveHelper_datatable_tabletools = undefined;
+
+var dataObjetivos;
+var objetivoId;
+
+var breakpointDefinition = {
+    tablet: 1024,
+    phone: 480
+};
+
+
+function initForm() {
+    //comprobarLogin();
+    // de smart admin
+    pageSetUp();
+    getVersionFooter();
+    //
+    $('#btnBuscar').click(buscarObjetivos());
+    $('#btnAlta').click(crearObjetivo());
+    $('#frmBuscar').submit(function () {
+        return false
+    });
+    //$('#txtBuscar').keypress(function (e) {
+    //    if (e.keyCode == 13)
+    //        buscarObjetivos();
+    //});
+    //
+    initTablaObjetivos();
+    // comprobamos parámetros
+    objetivoId = gup('ObjetivoId');
+    if (objetivoId !== '') {
+        // cargar la tabla con un único valor que es el que corresponde.
+        var data = {
+            id: objetivoId
+        }
+        // hay que buscar ese elemento en concreto
+        $.ajax({
+            type: "GET",
+            url: "api/objetivos/" + objetivoId,
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            success: function (data, status) {
+                // hay que mostrarlo en la zona de datos
+                var data2 = [data];
+                loadTablaObjetivos(data2);
+            },
+            error: errorAjax
+        });
+    }
+}
+
+function initTablaObjetivos() {
+    tablaCarro = $('#dt_objetivo').dataTable({
+        autoWidth: true,
+        preDrawCallback: function () {
+            // Initialize the responsive datatables helper once.
+            if (!responsiveHelper_dt_basic) {
+                responsiveHelper_dt_basic = new ResponsiveDatatablesHelper($('#dt_objetivo'), breakpointDefinition);
+            }
+        },
+        rowCallback: function (nRow) {
+            responsiveHelper_dt_basic.createExpandIcon(nRow);
+        },
+        drawCallback: function (oSettings) {
+            responsiveHelper_dt_basic.respond();
+        },
+        language: {
+            processing: "Procesando...",
+            info: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+            infoFiltered: "(filtrado de un total de _MAX_ registros)",
+            infoPostFix: "",
+            loadingRecords: "Cargando...",
+            zeroRecords: "No se encontraron resultados",
+            emptyTable: "Ningún dato disponible en esta tabla",
+            paginate: {
+                first: "Primero",
+                previous: "Anterior",
+                next: "Siguiente",
+                last: "Último"
+            },
+            aria: {
+                sortAscending: ": Activar para ordenar la columna de manera ascendente",
+                sortDescending: ": Activar para ordenar la columna de manera descendente"
+            }
+        },
+        data: dataObjetivos,
+        columns: [{
+            data: "nombre"
+            }, {
+                data: "categoria.nombre"
+            }, {
+                data: "tipo.nombre"
+            },
+         {
+            data: "objetivoId",
+            render: function (data, type, row) {
+                var bt1 = "<button class='btn btn-circle btn-danger btn-lg' onclick='deleteObjetivo(" + data + ");' title='Eliminar registro'> <i class='fa fa-trash-o fa-fw'></i> </button>";
+                var bt2 = "<button class='btn btn-circle btn-success btn-lg' onclick='editObjetivo(" + data + ");' title='Editar registro'> <i class='fa fa-edit fa-fw'></i> </button>";
+                var html = "<div class='pull-right'>" + bt1 + " " + bt2 + "</div>";
+                return html;
+            }
+        }]
+    });
+}
+
+function datosOK() {
+    //TODO: Incluir en la validación si el certificado figura en el almacén de certificados.
+    $('#frmBuscar').validate({
+        rules: {
+            txtBuscar: { required: true },
+        },
+        // Messages for form validation
+        messages: {
+            txtBuscar: {
+                required: 'Introduzca el texto a buscar'
+            }
+        },
+        // Do not change code below
+        errorPlacement: function (error, element) {
+            error.insertAfter(element.parent());
+        }
+    });
+    return $('#frmBuscar').valid();
+}
+
+function loadTablaObjetivos(data) {
+    var dt = $('#dt_objetivo').dataTable();
+    if (data !== null && data.length === 0) {
+        mostrarMensajeSmart('No se han encontrado registros');
+        $("#tbObjetivo").hide();
+    } else {
+        dt.fnClearTable();
+        dt.fnAddData(data);
+        dt.fnDraw();
+        $("#tbObjetivo").show();
+    }
+}
+
+function buscarObjetivos() {
+    var mf = function () {
+        if (!datosOK()) {
+            return;
+        }
+        // obtener el n.serie del certificado para la firma.
+        var aBuscar = $('#txtBuscar').val();
+        // enviar la consulta por la red (AJAX)
+        var data = {
+            "nombre": aBuscar
+        };
+        $.ajax({
+            type: "POST",
+            url: "api/objetivos-buscar",
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            success: function (data, status) {
+                // hay que mostrarlo en la zona de datos
+                loadTablaObjetivos(data);
+            },
+            error: errorAjax
+        });
+    };
+    return mf;
+}
+
+function crearObjetivo() {
+    var mf = function () {
+        var url = "ObjetivoDetalle.html?ObjetivoId=0";
+        window.open(url, '_self');
+    };
+    return mf;
+}
+
+function deleteObjetivo(id) {
+    // mensaje de confirmación
+    var mens = "¿Realmente desea borrar este registro?";
+    $.SmartMessageBox({
+        title: "<i class='fa fa-info'></i> Mensaje",
+        content: mens,
+        buttons: '[Aceptar][Cancelar]'
+    }, function (ButtonPressed) {
+        if (ButtonPressed === "Aceptar") {
+            var data = {
+                objetivoId: id
+            };
+            $.ajax({
+                type: "DELETE",
+                url: "api/objetivos/" + id,
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify(data),
+                success: function (data, status) {
+                    var fn = buscarObjetivos();
+                    fn();
+                },
+                error: errorAjax
+            });
+        }
+        if (ButtonPressed === "Cancelar") {
+            // no hacemos nada (no quiere borrar)
+        }
+    });
+}
+
+function editObjetivo(id) {
+    // hay que abrir la página de detalle de objetivo
+    // pasando en la url ese ID
+    var url = "ObjetivoDetalle.html?ObjetivoId=" + id;
+    window.open(url, '_self');
+}
+
+
